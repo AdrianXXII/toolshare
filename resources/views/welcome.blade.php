b8b7b55441d745b7f21dcd03eaf0b89ffb38c291
Adrian <adrianxxii@gmail.com>
2016-06-13 20:14:50 +0200

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Dempro</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Hier entsteht Toolshare 1.0</div>
+                <div class="content">Dominique Stampfli, Adrian Locher, Reto Aeschbacher</div>
             </div>
         </div>
     </body>
