7266fb8e53a047423a7c82a75c04c4e6229c97ae
RetoA <reto.aeschbacher@quickline.ch>
2016-07-05 22:51:28 +0200

    Arbeit an Authentifizierung und Registrierung

--- app/User.php
+++ app/User.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Model
 {
     //
     public $timestamps = false;
